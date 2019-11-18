3c9b26b814ecf151519e1c7d0b0f4ee9fc1520ff
Noel Mendoza <nmendozavd@gmail.com>
2019-11-18 15:26:24 -0800

    Completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, start, results) {
+  var results = results || [];
+  var start = start || document.body;
+
+  if (start.classList.contains(className)) {
+    results.push(start);
+  }
+
+  if (start.children) {
+    var children = start.children;
+
+    for (var i = 0; i < children.length; i++) {
+      getElementsByClassName(className, children[i], results);
+
+    }
+  }
+
+  return results;
+
 };
