50a8d7aaebe51c7d1cb4ffacfc35f6186a116168
Noel Mendoza <nmendozavd@gmail.com>
2019-11-18 13:43:31 -0800

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function (obj) {
+  
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (typeof obj === 'function' || typeof obj === 'undefined') {
+    return '';
+  }
+  // if array
+  if (Array.isArray(obj)) {
+    var stringArr = [];
+
+    obj.forEach(function (element) {
+      stringArr.push(stringifyJSON(element));
+    });
+    return '[' + stringArr.toString() + ']';
+  }
+
+  // if object
+  if (typeof obj === 'object') {
+    var stringObj = '';
+    var objKeys = Object.keys(obj);
+    objKeys.forEach(function (k) {
+      if (typeof k === 'function' || typeof k === 'undefined' || typeof obj[k] === 'function' || typeof obj[k] === 'undefined') {
+        return '';
+      } else {
+        stringObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ',';
+      }
+    });
+    return '{' + stringObj.slice(0, stringObj.length - 1) + '}';
+  }
+
+  return obj.toString();
 };
